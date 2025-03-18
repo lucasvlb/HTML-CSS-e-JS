5a844add5da54ad2ba74db4d615ab39529a0f737
lucasvlb <lucas.villalba97@outlook.com>
2025-03-17 22:13:39 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,22 @@
+// console.log(50+550);
+// console.log("Hello World");
+
+// var primeiroTipo;
+// let segundoTipo;
+// const terceiroTipo = 1;
+
+// let nome ="Lucas";
+// let idade ="27";
+
+// console.log(nome);
+// console.log(idade);
+
+// nome = idade;
+
+// console.log(nome);
+
+function hello(){
+    document.getElementById("texto").textContend ="hello world";
+
+}
+
