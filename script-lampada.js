e7a96b3a51ad0986ff76a511c402e6d0c810f0fb
lucasvlb <lucas.villalba97@outlook.com>
2025-03-19 20:39:28 -0300

    HTML-CSS-e-JS

---
+++ script-lampada.js
@@ -0,0 +1,12 @@
+function ligar(){
+    document.getElementById('Lampada').src="IMG/lampada-removebg-preview.png";
+
+
+
+}
+
+function desligar(){
+
+    document.getElementById('Lampada').src="IMG/lampada_apagada-removebg-preview.png";
+
+}
